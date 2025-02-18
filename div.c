aa53762cedf8e45f515442ada5e994fa29757718
Dev <dev.bharti@s.amity.edu>
2025-02-18 12:43:36 +0530

    Added floating point division support

--- div.c
+++ div.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 
 int main() {
-    int num1, num2, div;
+    float num1, num2, div;  // Change to float for decimal numbers
 
-    printf("Enter two integers: ");
-    scanf("%d %d", &num1, &num2);
+    printf("Enter two numbers: ");
+    scanf("%f %f", &num1, &num2);  // %f for floating-point input
 
+    // Check for division by zero
     if (num2 == 0) {
         printf("Error: Division by zero is not allowed.\n");
     } else {
-        div = num1 / num2;  
-        printf("div: %d\n", div);
+        div = num1 / num2;  // Perform floating-point division
+        printf("div: %.2f\n", div);  // Print result with 2 decimal places
     }
 
     return 0;
 }
+
