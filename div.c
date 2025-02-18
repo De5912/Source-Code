e35cacd2610629f61a751599c4ef188769d63aea
Dev <dev.bharti@s.amity.edu>
2025-02-18 12:38:53 +0530

    Added division by zero error handling

--- div.c
+++ div.c
@@ -6,9 +6,12 @@ int main() {
     printf("Enter two integers: ");
     scanf("%d %d", &num1, &num2);
 
-    div = num1 / num2;
-
-    printf("div: %d\n", div);
+    if (num2 == 0) {
+        printf("Error: Division by zero is not allowed.\n");
+    } else {
+        div = num1 / num2;  
+        printf("div: %d\n", div);
+    }
 
     return 0;
 }
