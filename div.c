bb2e43437bea7119e982b5ffa5a7068c669d1a1f
Dev <dev.bharti@s.amity.edu>
2025-02-25 11:14:47 +0530

    Added loop for multiple calculations

--- div.c
+++ div.c
@@ -2,23 +2,31 @@
 
 int main() {
     float num1, num2, div;  
-    int check; 
+    int check;
+    char choice;
 
-    printf("Enter two numbers: ");
-    check = scanf("%f %f", &num1, &num2); 
+    do {
+        printf("Enter two numbers: ");
+        check = scanf("%f %f", &num1, &num2);
 
-    if (check != 2) {
-        printf("Error: Invalid input! Please enter numeric values.\n");
-        return 1;
-    }
+        if (check != 2) {
+            printf("Error: Invalid input! Please enter numeric values.\n");
+            while (getchar() != '\n'); 
+            continue;  
+        }
 
-    if (num2 == 0) {
-        printf("Error: Division by zero is not allowed.\n");
-    } else {
-        div = num1 / num2;  
-        printf("div: %.2f\n", div);  
-    }
+        if (num2 == 0) {
+            printf("Error: Division by zero is not allowed.\n");
+        } else {
+            div = num1 / num2;
+            printf("div: %.2f\n", div);
+        }
 
+        printf("Do you want to perform another calculation? (y/n): ");
+        scanf(" %c", &choice);
+        
+    } while (choice == 'y' || choice == 'Y');
+
+    printf("Goodbye!\n");
     return 0;
-}
 
