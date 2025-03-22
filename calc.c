be29b71e478696e14bb582730f53d066fae91de3
Dev <dev.bharti@s.amity.edu>
2025-03-22 10:51:14 +0530

    Calculator 2.0

--- calc.c
+++ calc.c
@@ -1,66 +1,103 @@
 #include <windows.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-// Function Prototypes
+#define SCREEN_ID 1
+#define BTN_0 10
+#define BTN_1 11
+#define BTN_2 12
+#define BTN_3 13
+#define BTN_4 14
+#define BTN_5 15
+#define BTN_6 16
+#define BTN_7 17
+#define BTN_8 18
+#define BTN_9 19
+#define BTN_ADD 20
+#define BTN_SUB 21
+#define BTN_MUL 22
+#define BTN_DIV 23
+#define BTN_EQ  24
+#define BTN_CLR 25
+
+char expression[256] = ""; // Stores input like "12+3*5"
+HWND hScreen; // Display screen
+
 LRESULT CALLBACK WindowProcedure(HWND, UINT, WPARAM, LPARAM);
 
-// Global Variables for UI Elements
-HWND hEdit1, hEdit2, hResult, hButtonAdd, hButtonSub;
+void AddButton(HWND hwnd, int id, char* text, int x, int y) {
+    CreateWindow("button", text, WS_VISIBLE | WS_CHILD, x, y, 50, 50, hwnd, (HMENU)id, NULL, NULL);
+}
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+void AddButtons(HWND hwnd) {
+    hScreen = CreateWindow("edit", "", WS_VISIBLE | WS_CHILD | WS_BORDER | ES_RIGHT, 10, 10, 230, 30, hwnd, (HMENU)SCREEN_ID, NULL, NULL);
+    
+    int x = 10, y = 50;
+    int ids[] = {BTN_7, BTN_8, BTN_9, BTN_DIV, BTN_4, BTN_5, BTN_6, BTN_MUL, BTN_1, BTN_2, BTN_3, BTN_SUB, BTN_0, BTN_CLR, BTN_EQ, BTN_ADD};
+    char* labels[] = {"7", "8", "9", "/", "4", "5", "6", "*", "1", "2", "3", "-", "0", "C", "=", "+"};
+
+    for (int i = 0; i < 16; i++) {
+        AddButton(hwnd, ids[i], labels[i], x, y);
+        x += 60;
+        if ((i + 1) % 4 == 0) { x = 10; y += 60; }
+    }
+}
+
+void UpdateScreen(HWND hwnd) {
+    SetWindowText(hScreen, expression);
+}
+
+void Calculate() {
+    int result = 0;
+    // Basic parsing logic (can be improved)
+    result = atoi(expression); // Convert to int (simple logic, replace with better parser)
+    sprintf(expression, "%d", result);
+}
+
+LRESULT CALLBACK WindowProcedure(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp) {
+    switch (msg) {
+        case WM_CREATE:
+            AddButtons(hwnd);
+            break;
+        case WM_COMMAND:
+            if (wp >= BTN_0 && wp <= BTN_9) {
+                char num[2] = {(char)(wp - BTN_0 + '0'), '\0'};
+                strcat(expression, num);
+            } else if (wp == BTN_ADD) strcat(expression, "+");
+            else if (wp == BTN_SUB) strcat(expression, "-");
+            else if (wp == BTN_MUL) strcat(expression, "*");
+            else if (wp == BTN_DIV) strcat(expression, "/");
+            else if (wp == BTN_EQ) Calculate();
+            else if (wp == BTN_CLR) strcpy(expression, "");
+
+            UpdateScreen(hwnd);
+            break;
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            break;
+        default:
+            return DefWindowProc(hwnd, msg, wp, lp);
+    }
+    return 0;
+}
+
+int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR args, int ncmdshow) {
     WNDCLASS wc = {0};
-    wc.hInstance = hInstance;
-    wc.lpszClassName = "CalculatorApp";
-    wc.lpfnWndProc = WindowProcedure;
-    wc.hbrBackground = (HBRUSH)(COLOR_WINDOW+1);
+    wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
     wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc.hInstance = hInst;
+    wc.lpszClassName = "Calculator";
+    wc.lpfnWndProc = WindowProcedure;
 
     RegisterClass(&wc);
-    HWND hwnd = CreateWindow("CalculatorApp", "Simple Calculator", WS_OVERLAPPEDWINDOW, 100, 100, 300, 200, NULL, NULL, hInstance, NULL);
-    ShowWindow(hwnd, nCmdShow);
-    UpdateWindow(hwnd);
+    HWND hwnd = CreateWindow("Calculator", "WinAPI Calculator", WS_OVERLAPPEDWINDOW | WS_VISIBLE, 100, 100, 270, 350, NULL, NULL, hInst, NULL);
 
-    MSG msg;
+    MSG msg = {0};
     while (GetMessage(&msg, NULL, 0, 0)) {
         TranslateMessage(&msg);
         DispatchMessage(&msg);
     }
-    return (int) msg.wParam;
+    return 0;
 }
 
-// Handles Events
-LRESULT CALLBACK WindowProcedure(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
-    switch (msg) {
-        case WM_CREATE:
-            // Create Input Boxes
-            hEdit1 = CreateWindow("EDIT", "", WS_CHILD | WS_VISIBLE | WS_BORDER, 50, 20, 80, 20, hwnd, NULL, NULL, NULL);
-            hEdit2 = CreateWindow("EDIT", "", WS_CHILD | WS_VISIBLE | WS_BORDER, 150, 20, 80, 20, hwnd, NULL, NULL, NULL);
-            hResult = CreateWindow("STATIC", "Result: ", WS_CHILD | WS_VISIBLE, 50, 50, 180, 20, hwnd, NULL, NULL, NULL);
-            
-            // Create Buttons
-            hButtonAdd = CreateWindow("BUTTON", "+", WS_CHILD | WS_VISIBLE, 50, 80, 40, 30, hwnd, (HMENU)1, NULL, NULL);
-            hButtonSub = CreateWindow("BUTTON", "-", WS_CHILD | WS_VISIBLE, 100, 80, 40, 30, hwnd, (HMENU)2, NULL, NULL);
-            break;
-
-        case WM_COMMAND:
-            if (LOWORD(wParam) == 1 || LOWORD(wParam) == 2) {
-                char text1[10], text2[10];
-                int num1, num2, result;
-                GetWindowText(hEdit1, text1, 10);
-                GetWindowText(hEdit2, text2, 10);
-                num1 = atoi(text1);
-                num2 = atoi(text2);
-                
-                result = (LOWORD(wParam) == 1) ? (num1 + num2) : (num1 - num2);
-
-                char resText[20];
-                sprintf(resText, "Result: %d", result);
-                SetWindowText(hResult, resText);
-            }
-            break;
-
-        case WM_DESTROY:
-            PostQuitMessage(0);
-            break;
-    }
-    return DefWindowProc(hwnd, msg, wParam, lParam);
-}
